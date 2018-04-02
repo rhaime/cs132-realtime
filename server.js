8d04bc49a9378a906cd87668e72aac7833b1d231
rkim <rhaime.kim@gmail.com>
2018-04-02 11:20:41 -0400

    not showing active users

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ app.set('views', __dirname + '/templates');
 app.set('view engine', 'html');
 
 var messages = []
+var roomNamee = null;
 
 // show home.html
 app.get('/', function(req, res){
@@ -40,54 +41,37 @@ app.get('/chatroom/:roomName', function(req, res){
 
 io.sockets.on('connection', function(socket){
 
-    // on client side we sent back the roomname and the nickname
+    // receive roomname and nickname from client side
     socket.on('join', function(roomName, nickname, callback){
-      
-      // join the room name that is passed
       socket.join(roomName);
-
-      // add a nickname property to the socket and set it to emitted nickname
+      socket.roomName = roomName;
       socket.nickname = nickname;
 
       // send back the messages already saved in that room, if any
-      conn.query('SELECT * FROM message WHERE room=$1', function(error, data){
-        // each item in this array is one message
-        // messages = data.rows;
-
+      conn.query('SELECT * FROM message WHERE room=$1', [roomName], function(error, data){
         if (error){
           console.log("FAILED to add to database");
           res.sendStatus(500);
         } else {
           messages = data.rows;
-          socket.emit('join', messages);
+          io.sockets.in(roomName).emit('join', messages);
         }
-
       });
-
-      // send back messages, where each item is a single message with all 3 elements
-      //callback(messages);
-      broadcastMemberJoined();
+      io.sockets.in(roomName).emit('newMember', nickname);
     });
 
-    // socket.emit('join', function(messages){
-    //   console.log(messages);
-    // });
-
-    socket.on('changedNickname', function(nickname){
-        socket.nickname = nickname;
-        broadcastMemberJoined();
+    socket.on('changedNickname', function(roomName, oldNickname, newNickname){
+        socket.nickname = newNickname;
+        io.sockets.in(roomName).emit('changedMember', oldNickname, newNickname);
     });
 
     // add incoming message to database
     socket.on('message', function(message){
 
-      var roomNamee = Object.keys(io.sockets.adapter.sids[socket.id])[1];
+      roomNamee = Object.keys(io.sockets.adapter.sids[socket.id])[1];
       var nicknamee = socket.nickname
       var date = new Date();
       var timee = date.getHours() + ":" + date.getMinutes();
-
-      // add to database
-      //conn.query('INSERT INTO message (room, nickname, body, time) VALUES($1, $2, $3, $4)', [roomNamee, nicknamee, message, timee]);
       
       // add to database
       conn.query('INSERT INTO message (room, nickname, body, time) VALUES($1, $2, $3, $4)', [roomNamee, nicknamee, message, timee], function(error, data) {
@@ -100,26 +84,18 @@ io.sockets.on('connection', function(socket){
         }
       });
 
-  //       conn.query('INSERT INTO message (room, nickname, body, time) VALUES($1, $2, $3, $4)', [roomm, nicknamee, bodyy, timee], function(error, data) { 
-  //     if(error){
-  //         console.log("failed to add to database....")
-  //         res.sendStatus(500);
-  //     } else {
-  //         console.log("added")
-  //         res.json({nickname: nicknamee, message: bodyy, time:timee})
-  //     }
-  // })};
-
       // send back to roomname the message, along with other info to display
       io.sockets.in(roomNamee).emit('message', nicknamee, message, timee);
     
     });
 
+// emit the name of person leaving
     socket.on('disconnect', function(){
         // Leave the room!
         // notify others
 
-        broadcastMemberJoined();
+        // instead of calling a function, instead emit on 'exitMember'
+        //io.sockets.in(socket.roomName).emit('exitMember', nickname);
     });
 
     socket.on('error', function(){
@@ -127,35 +103,7 @@ io.sockets.on('connection', function(socket){
         // Maybe you can try to notify users that an error occured and log the error as well.
     });
 
-})
-
-function broadcastMemberJoined(roomName, nickname) {
-    io.sockets.in(roomName).emit('newMember', nickname);
-
-}
-
-// app.post('/:roomName/messages', saveMessage);
-
-// // change to socket (on message, save the message,)
-// // add chat information to database
-
-// function saveMessage(req, res) {
-
-//   var roomm = req.params.roomName;
-//   var nicknamee = req.body.nickname;
-//   var bodyy = req.body.message;
-//   var date = new Date();
-//   var timee = date.getHours() + ":" + date.getMinutes();
-
-//   conn.query('INSERT INTO message (room, nickname, body, time) VALUES($1, $2, $3, $4)', [roomm, nicknamee, bodyy, timee], function(error, data) { 
-//       if(error){
-//           console.log("failed to add to database....")
-//           res.sendStatus(500);
-//       } else {
-//           console.log("added")
-//           res.json({nickname: nicknamee, message: bodyy, time:timee})
-//       }
-//   })};
+});
 
 function generateRoomIdentifier() {
   var chars = 'ABCDEFGHJKLMNPQRSTUVWXYZ23456789';
